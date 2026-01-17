7c05270794e67aa57a2e6ff779ed73b4a6401aa3
huny <sehoon99@naver.com>
2026-01-17 23:11:27 +0900

    please2

--- terraform/vpc/bhunyd_apnortheast2/remote_state.tf
+++ terraform/vpc/bhunyd_apnortheast2/remote_state.tf
@@ -1,5 +1,5 @@
 data "terraform_remote_state" "s3" {
   backend = "s3"
   #   config  = merge(var.remote_state.s3.zerone-id)
-  config = merge(var.remote_state.s3.bhuny-id, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+  config = merge(var.remote_state.s3.bhuny-id, { "role_arn" = var.assume_role_arn } )
 }
