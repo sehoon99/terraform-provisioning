a9524a9d9c78d183e7d46105ba61d98126d0b38c
huny <sehoon99@naver.com>
2026-01-18 20:09:34 +0900

    real eip, nat down

--- terraform/vpc/bhunyd_apnortheast2/remote_state.tf
+++ terraform/vpc/bhunyd_apnortheast2/remote_state.tf
@@ -1,5 +1,19 @@
+# data "terraform_remote_state" "s3" {
+#   backend = "s3"
+#   #   config  = merge(var.remote_state.s3.zerone-id)
+#   config = merge(var.remote_state.s3.bhuny-id, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+# }
 data "terraform_remote_state" "s3" {
   backend = "s3"
-  #   config  = merge(var.remote_state.s3.zerone-id)
-  config = merge(var.remote_state.s3.bhuny-id, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+
+  config = {
+    bucket   = "bhuny-id-apnortheast2-tfstate"
+    key      = "provisioning/terraform/vpc/bhunyd_apnortheast2/terraform.tfstate"
+    region   = "ap-northeast-2"
+    
+    # 어드민 권한이 있는 역할을 명시적으로 사용
+    assume_role = {
+      role_arn = "arn:aws:iam::633309913072:role/atlantis-bhuny-id-admin"
+    }
+  }
 }
