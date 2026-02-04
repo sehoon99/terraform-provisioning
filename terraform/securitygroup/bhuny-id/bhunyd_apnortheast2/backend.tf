f20fcb6e3145beb9334134c7c3df9a1153613ce4
huny <sehoon99@naver.com>
2026-02-04 17:50:59 +0900

    iam + sg

---
+++ terraform/securitygroup/bhuny-id/bhunyd_apnortheast2/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket         = "bhuny-id-apnortheast2-tfstate"
+    key            = "provisioning/terraform/securitygroup/bhuny-id/bhunyd_apnortheast2/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
