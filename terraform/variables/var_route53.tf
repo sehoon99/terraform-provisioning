afc2d663763d3a3b2964260a02d34afb607e87ae
huny <sehoon99@naver.com>
2026-02-05 15:35:02 +0900

    Update var_route53.tf with ai_news_tracker_com zone

--- terraform/variables/var_route53.tf
+++ terraform/variables/var_route53.tf
@@ -1,6 +1,6 @@
 variable "r53_variables" {
   default = {
-    terraform_devart_tv_zone_id            = "Z09601362JG8MSL8LJAHR"
-    star_terraform_devart_tv_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:066346343248:certificate/10d501d4-a8c8-4358-9996-98060a907e47"
+    ai_news_tracker_com_zone_id            = "ZZ07035479QJK8Q5DK72E"
+    ai_news_tracker_com_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:633309913072:certificate/10d501d4-a8c8-4358-9996-98060a907e47"
   }
 }
