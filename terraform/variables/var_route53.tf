35d15bea16e64afc8b4b6c87a0c321fe475d153f
huny <sehoon99@naver.com>
2026-02-05 16:15:59 +0900

    provisioning jenkins

--- terraform/variables/var_route53.tf
+++ terraform/variables/var_route53.tf
@@ -2,6 +2,6 @@ variable "r53_variables" {
   default = {
     ai_news_tracker_com_zone_id            = "Z07035479QJK8Q5DK72E"
     
-    ai_news_tracker_com_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:633309913072:certificate/10d501d4-a8c8-4358-9996-98060a907e47"
+    ai_news_tracker_com_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:633309913072:certificate/414253d7-1cdd-48e5-b76c-47c60a0b7edc"
   }
 }
