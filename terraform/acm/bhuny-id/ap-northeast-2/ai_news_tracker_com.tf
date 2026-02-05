10570257268d691513bb7d8418d6f4d416a08de7
huny <sehoon99@naver.com>
2026-02-05 15:24:01 +0900

    add-acm3

--- terraform/acm/bhuny-id/ap-northeast-2/ai_news_tracker_com.tf
+++ terraform/acm/bhuny-id/ap-northeast-2/ai_news_tracker_com.tf
@@ -3,6 +3,7 @@ module "ai_news_tracker_com" {
 
   domain_name = "*.ai-news-tracker.com"
   zone_id     = var.r53_variables.ai_news_tracker_com_zone_id
+  
   subject_alternative_names          = ["ai-news-tracker.com"]
   validation_allow_overwrite_records = true
   wait_for_validation                = true
