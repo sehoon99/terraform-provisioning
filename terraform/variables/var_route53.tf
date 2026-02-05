2f32214627d3003e49d7f6f517a6927de23207fb
huny <sehoon99@naver.com>
2026-02-05 15:41:07 +0900

    add-acm7

--- terraform/variables/var_route53.tf
+++ terraform/variables/var_route53.tf
@@ -1,6 +1,7 @@
 variable "r53_variables" {
   default = {
-    ai_news_tracker_com_zone_id            = "ZZ07035479QJK8Q5DK72E"
+    ai_news_tracker_com_zone_id            = "Z07035479QJK8Q5DK72E"
+    
     ai_news_tracker_com_acm_apne2_arn = "arn:aws:acm:ap-northeast-2:633309913072:certificate/10d501d4-a8c8-4358-9996-98060a907e47"
   }
 }
